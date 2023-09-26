fd729285440f30070703822ce7c07efb30a2566e
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2023-09-26 20:36:43 +0200

    adding the database setup and first table

---
+++ db/DB_fill.sql
@@ -0,0 +1,27 @@
+USE `exercise-looper` ;
+
+insert into exercises (id, name) values (1, 'Tax Accountant');
+insert into exercises (id, name) values (2, 'Safety Technician II');
+insert into exercises (id, name) values (3, 'Geologist I');
+insert into exercises (id, name) values (4, 'Structural Analysis Engineer');
+insert into exercises (id, name) values (5, 'Physical Therapy Assistant');
+insert into exercises (id, name) values (6, 'Web Designer III');
+insert into exercises (id, name) values (7, 'Teacher');
+insert into exercises (id, name) values (8, 'Quality Engineer');
+insert into exercises (id, name) values (9, 'Speech Pathologist');
+insert into exercises (id, name) values (10, 'Human Resources Assistant III');
+insert into exercises (id, name) values (11, 'Professor');
+insert into exercises (id, name) values (12, 'Analog Circuit Design manager');
+insert into exercises (id, name) values (13, 'Administrative Assistant III');
+insert into exercises (id, name) values (14, 'Payment Adjustment Coordinator');
+insert into exercises (id, name) values (15, 'Registered Nurse');
+insert into exercises (id, name) values (16, 'Data Coordinator');
+insert into exercises (id, name) values (17, 'Office Assistant II');
+insert into exercises (id, name) values (18, 'Nuclear Power Engineer');
+insert into exercises (id, name) values (19, 'Environmental Specialist');
+insert into exercises (id, name) values (20, 'Account Coordinator');
+insert into exercises (id, name) values (21, 'Budget/Accounting Analyst IV');
+insert into exercises (id, name) values (22, 'Project Manager');
+insert into exercises (id, name) values (23, 'Nuclear Power Engineer');
+insert into exercises (id, name) values (24, 'Geologist II');
+insert into exercises (id, name) values (25, 'Recruiter');
