0f1a8537df2bfe3368d3a8eaca1e8095d9699bd1
JuilletMikael <mikael.juillet@eduvaud.ch>
2023-09-27 10:15:59 +0200

    create renderer

---
+++ src/Renderer.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace model\class;
+
+/**
+ * This class is designed to manage the rendering process.
+ */
+class Renderer
+{
+    private string $render;
+    private string $httpResponse;
+
+    /**
+     * This constructor allow to create a new renderer object
+     *
+     * @param string $render the content of the view
+     * @param string $httpResponse the http response (actually only the code response)
+     */
+    public function __construct(string $render, string $httpResponse)
+    {
+       $this->render = $render;
+       $this->httpResponse = $httpResponse;
+    }
+
+    /**
+     *  This method is used to send information response.
+     *
+     * @return void
+     */
+    public function send(): void
+    {
+        //TODO : Add header on the response.
+        header(' ', false, $this->httpResponse);
+        echo($this->render);
+    }
+}
