9a30987a5cb84193816bf9bc36c683165db0a0e0
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2023-09-27 10:57:38 +0200

    adding the page view and the card components with hard data

---
+++ src/view/components/exerciseCard.php
@@ -0,0 +1,11 @@
+<?php
+
+function card($title)
+{
+    echo "
+        <div class='card'>
+            <span class='title'>$title</span>
+            <button class='btn bg-purple'>take it</button>
+        </div>
+    ";
+}
--- src/view/layout.php
+++ src/view/layout.php
@@ -1,5 +1,3 @@
-<?php
-
 <!doctype html>
 <html lang="fr">
 <head>
@@ -7,11 +5,15 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="icon" href="../../public/images/logo.png" type="image/x-icon"/>
+
+    <link rel="icon" href='/public/images/logo.png' type="image/x-icon"/>
+
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="../../public/css/layout.css">
+
+    <link rel='stylesheet' href='/public/css/layout.css'>
+    <?=$style?>
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
             integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
@@ -23,7 +25,7 @@
             integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
             crossorigin="anonymous"></script>
 
-    <title>ExerciseLooper</title>
+    <title><?=$title?></title>
 </head>
 
 <body>
@@ -31,14 +33,14 @@
 <nav class="fixed-top bg-purple">
     <div class="m-auto">
         <a href="#">
-            <img src="../../public/images/logo.png" width="50" height="50" alt="Exercise looper">
+            <img src="/public/images/logo.png" width="50" height="50" alt="Exercise looper">
         </a>
     </div>
 </nav>
 
 <!-- PAGE CONTENT -->
-<div class="container pt-nav pt-standard m-auto">
-
+<div class="page-container pt-nav pt-standard m-auto">
+    <?=$content?>
 </div>
 
 </body>
