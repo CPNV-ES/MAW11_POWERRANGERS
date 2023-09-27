2f55d6fdce53de3caaa8b9c259dad36c9a7e5e44
Sebastien Moraz <sebastien.moraz@cpnv.ch>
2023-09-27 07:20:56 +0200

    add Router class

--- src/Router.php
+++ src/Router.php
@@ -4,8 +4,54 @@ namespace model\class;
 
 class Router
 {
-    public function __construct()
+    private string $layout = "views/layout";
+    private array $routes = [];
+    private string $handler;
+    private string $status_code;
+
+
+    public function add(string $route, string $method, string $handler, string $status_code = "200") : void
     {
-        echo "Init Router";
+        $this->routes[] = [
+            "route" => $route,
+            "method" => $method,
+            "handler" => $handler
+        ];
     }
+
+    public function run(string $routeRequest, string $methodRequest) : void
+    {
+        foreach ($this->routes as $route) {
+            if ($route["route"] === $routeRequest && $route["method"] === $methodRequest) {
+                $this->handler = $route["handler"];
+            }
+        }
+        if (!isset($this->handler)) {
+            $this->handler = "views/error";
+            $this->status_code = "404";
+        }
+    }
+
+    public function getRoutes(): array
+    {
+        return $this->routes;
+    }
+
+    public function getHandler(): string
+    {
+        return $this->handler;
+    }
+
+    public function getStatusCode(): string
+    {
+        return $this->status_code;
+    }
+
+    public function getLayout(): string
+    {
+        return $this->layout;
+    }
+
+
+
 }
--- tests/TestRouter.php
+++ tests/TestRouter.php
@@ -15,4 +15,41 @@ class TestRouter extends TestCase
         $router = new Router();
         $this->assertInstanceOf(Router::class, $router);
     }
+
+    public function testAddSuccess()
+    {
+        $router = new Router();
+        $router->add("/", "GET", "views/exercise");
+
+        $routes = $router->getRoutes();
+
+        $routeExists = false;
+        foreach ($routes as $route) {
+            if ($route['route'] === '/' && $route['method'] === 'GET' && $route['handler'] === 'views/exercise') {
+                $routeExists = true;
+                break;
+            }
+        }
+        $this->assertTrue($routeExists, 'the route has not been added');
+    }
+
+    public function testRunSuccess()
+    {
+        $router = new Router();
+        $router->add("/", "GET", "views/exercise");
+        $router->run("/", "GET");
+
+        $this->assertEquals("views/exercise", $router->getHandler(), 'the handler is not correct');
+    }
+
+    public function testRunError()
+    {
+        $router = new Router();
+        $router->add("/", "GET", "views/exercise");
+        $router->run("/error", "GET");
+
+        $this->assertEquals("views/error", $router->getHandler(), 'the handler is not correct');
+        $this->assertEquals("404", $router->getStatusCode(), 'the status code is not correct');
+    }
+
 }
