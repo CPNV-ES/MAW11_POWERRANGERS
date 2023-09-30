0311bcacc3b02d1a5251f760d047382690b29270
JuilletMikael <mikael.juillet@eduvaud.ch>
2023-09-30 14:42:40 +0200

    Updating css path & remove unecesary php tag

--- src/view/pages/exercises.php
+++ src/view/pages/exercises.php
@@ -2,7 +2,7 @@
 $title = "Exercises";
 $style = "<link rel='stylesheet' href='./css/pages/exercises.css'>";
 
-require_once dirname(__FILE__).'/../components/exerciseCard.php';
+require_once dirname(__FILE__) . '/../components/exerciseCard.php';
 
 //HARD exercises
 $exercises = ["a", "Linux base commands", "CLD2 Cloud Services"];
@@ -11,5 +11,5 @@ ob_start();
  array_map('card', $exercises);
 
 $content = ob_get_clean();
-require dirname(__FILE__)."/../layout.php";
+require dirname(__FILE__) . "/../layout.php";
 ?>
