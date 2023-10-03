cee63de81730842b4f6eb783ded50482cc07265a
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2023-10-03 21:55:41 +0200

    Feat : add the error 500 view to upgrade the UX

---
+++ src/view/errors/500.php
@@ -0,0 +1,15 @@
+<?php
+
+$title = "404";
+$style = "<link rel='stylesheet' href='./css/pages/error.css'>";
+
+ob_start();
+?>
+
+    <h1>500</h1>
+    <span>Oh no! Something went wrong on our end. We're working to fix it as soon as possible. In the meantime, you can try refreshing the page or come back later.</span>
+
+<?php
+$content = ob_get_clean();
+require __DIR__ . "/../layout.php";
+?>
