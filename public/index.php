e54385654aa7b792e8743ae69632bcc2b6d6853e
Sebastien Moraz <sebastien.moraz@cpnv.ch>
2023-10-03 22:04:37 +0200

    Fix : add error 500 gestion for index

--- public/index.php
+++ public/index.php
@@ -9,37 +9,53 @@ require_once __DIR__.'/../src/Router.php';
 require_once __DIR__.'/../src/Handler.php';
 require_once __DIR__.'/../src/Renderer.php';
 
-// Load environment variable
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "../..");
-$dotenv->load();
+try {
+    // Load environment variable
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "../..");
+    $dotenv->load();
 
-// Get requested route
-$route = $_SERVER["REQUEST_URI"];
-if (!empty($_SERVER["QUERY_STRING"])) {
-    $route = substr($route, 0, strlen($_SERVER["REQUEST_URI"])-strlen($_SERVER["QUERY_STRING"])-1);
+    // Get requested route
+    $route = $_SERVER["REQUEST_URI"];
+    if (!empty($_SERVER["QUERY_STRING"])) {
+        $route = substr($route, 0, strlen($_SERVER["REQUEST_URI"]) - strlen($_SERVER["QUERY_STRING"]) - 1);
+    }
+    $method = $_SERVER["REQUEST_METHOD"];
+
+    $router = new Router();
+
+    //----------------------------------------//
+    // Add your routes here
+
+    $router->add("/exercises", "GET", "controller/exercises");
+
+    //----------------------------------------//
+
+    $router->run($route, $method);
+    $handle = $router->getHandler();
+    $status_code = $router->getStatusCode();
+
+    //----------------------------------------//
+    // handler
+    $handler = new Handler($handle, $status_code);
+    $handler->handle();
+    $render = $handler->getRender();
+    $status_code = $handler->getStatusCode();
+    //----------------------------------------//
+    // Renderer
+    $renderer = new Renderer($render, $status_code);
+    $renderer->send();
+
+} catch (Exception) {
+    $handle = "view/errors";
+    $status_code = 500;
+    //----------------------------------------//
+    // handler
+    $handler = new Handler($handle, $status_code);
+    $handler->handle();
+    $render = $handler->getRender();
+    $status_code = $handler->getStatusCode();
+    //----------------------------------------//
+    // Renderer
+    $renderer = new Renderer($render, $status_code);
+    $renderer->send();
 }
-$method = $_SERVER["REQUEST_METHOD"];
-
-$router = new Router();
-
-//----------------------------------------//
-// Add your routes here
-
-$router->add("/exercises", "GET", "controller/exercises");
-
-//----------------------------------------//
-
-$router->run($route, $method);
-$handle = $router->getHandler();
-$status_code = $router->getStatusCode();
-
-//----------------------------------------//
-// handler
-$handler = new Handler($handle, $status_code);
-$handler->handle();
-$render = $handler->getRender();
-$status_code = $handler->getStatusCode();
-//----------------------------------------//
-// Renderer
-$renderer = new Renderer($render, $status_code);
-$renderer->send();
