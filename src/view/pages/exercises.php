964ae4f87802db342c430b3fc3f4b6c8d04aca62
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2023-10-04 10:24:20 +0200

    Fix : fixe the merging char generated by git in exercises views

--- src/view/pages/exercises.php
+++ src/view/pages/exercises.php
@@ -5,24 +5,21 @@ $title = "Exercises";
 $style = "<link rel='stylesheet' href='./css/pages/exercises.css'>";
 
 //load card component
-require_once __DIR__.'/../components/exerciseCard.php';
+require_once __DIR__ . '/../components/exerciseCard.php';
 
 ob_start();
-<<<<<<< HEAD
 ?>
-<ul>
-<?php
- array_map('card', $exercises);
-?>
-</ul>
-<?php
-=======
+    <ul>
+        <?php
 
-//make cards with data from database
-array_map('card', $exercises);
+        //make cards with data from database
+        array_map('card', $exercises);
+        ?>
 
->>>>>>> 3e6334827938d786e663a4384708c46e3b811afc
+    </ul>
+<?php
+
 $content = ob_get_clean();
 
 //load layout
-require __DIR__."/../layout.php";
+require __DIR__ . "/../layout.php";
