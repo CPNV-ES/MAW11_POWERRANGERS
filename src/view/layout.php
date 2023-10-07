4ea8782fd97ee9e1f84c28de194682bba9ba0b6c
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2023-10-07 13:54:12 +0200

    Feat : adding the redirection from navbar to home page

--- src/view/layout.php
+++ src/view/layout.php
@@ -39,7 +39,7 @@ if (isset($nav)) {
 } else { ?>
     <nav class="fixed-top bg-purple">
         <div class="m-auto">
-            <a href="#">
+            <a href="/">
                 <img src="images/logo.png" width="50" height="50" alt="Exercise looper">
             </a>
         </div>
