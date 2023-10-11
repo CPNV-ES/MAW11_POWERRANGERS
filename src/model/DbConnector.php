beaad638166bc176ff5d9ae81d2e4fc707cfc4f5
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2023-10-11 15:05:21 +0200

    Fix : change the query in DBConnector to avoid SQL Injection

--- src/model/DbConnector.php
+++ src/model/DbConnector.php
@@ -6,41 +6,44 @@ use PDO;
 
 /**
  * Class DbConnector
+ *
  * @package model\class
  */
 class DbConnector
 {
-        //attributes
-        private string $dbHost;
-        private string $dbName;
-        private string $dbUser;
-        private string $dbPass;
-        private PDO $db;
+    //attributes
+    private string $dbHost;
+    private string $dbName;
+    private string $dbUser;
+    private string $dbPass;
+    private PDO $db;
 
-        //constructor
-        public function __construct(string $dbHost, string $dbName, string $dbUser, string $dbPass)
-        {
-            //set attributes
-            $this->dbHost = $dbHost;
-            $this->dbName = $dbName;
-            $this->dbUser = $dbUser;
-            $this->dbPass = $dbPass;
+    //constructor
+    public function __construct(string $dbHost, string $dbName, string $dbUser, string $dbPass)
+    {
+        //set attributes
+        $this->dbHost = $dbHost;
+        $this->dbName = $dbName;
+        $this->dbUser = $dbUser;
+        $this->dbPass = $dbPass;
 
-            //connect to database
-            $this->db = new PDO(
-                "mysql:host=$this->dbHost;dbname=$this->dbName;charset=utf8",
-                $this->dbUser,
-                $this->dbPass
-            );
-        }
+        //connect to database
+        $this->db = new PDO(
+            "mysql:host=$this->dbHost;dbname=$this->dbName;charset=utf8",
+            $this->dbUser,
+            $this->dbPass
+        );
+    }
 
-        /**
-         * @param string $query
-         * @return false|array
-         */
-        public function Query(string $query) : false|array
-        {
-            //execute query to database
-             return $this->db->query($query)->fetchAll(PDO::FETCH_CLASS);
-        }
+    /**
+     * @param string $query
+     * @return false|array
+     */
+
+    public function query(string $template, array $params = []): false|array
+    {
+        $sth = $this->db->prepare($template);
+        $sth->execute($params);
+        return $sth->fetchAll(PDO::FETCH_CLASS);
+    }
 }
