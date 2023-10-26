b16aa880d88676cc864d83863b36143cbb8e7f36
JuilletMikael <mikael.juillet@eduvaud.ch>
2023-10-26 12:20:30 +0200

    Feat : add get fieldsTypes and update varriables names

--- src/controller/fields.php
+++ src/controller/fields.php
@@ -2,8 +2,10 @@
 
 // load model for exercises
 require_once __DIR__."/../model/fields.php";
+require_once __DIR__."/../model/fieldsTypes.php";
 
-$exercises = getFieldsByExercise();
+$fields = getFieldsByExercise(1);
+$fieldsTypes = getFieldsTypes();
 
 // load view for exercises
 require_once __DIR__."/../view/pages/fields.php";
