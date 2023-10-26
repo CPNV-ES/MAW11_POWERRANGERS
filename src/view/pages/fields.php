8ac9ffbed5b42542cfd5ad8a399b3d6d1f23f304
JuilletMikael <mikael.juillet@eduvaud.ch>
2023-10-26 12:21:20 +0200

    Feat : update field page to have dynamic fields types

--- src/view/pages/fields.php
+++ src/view/pages/fields.php
@@ -2,19 +2,21 @@
 
 //initialize page variables
 $title = "Exercises";
-
-//load card component
-require_once __DIR__ . '/../components/exerciseCard.php';
+$styles = array("<link rel='stylesheet' href='./css/components/exerciseCard.css'>");
 
 ob_start();
 ?>
-
     <form action="/">
         <label for="Label">Label</label><br>
         <input type="text" id="Label" name="Label"><br>
+
         <label for="fieldType">Value kind</label><br>
-        <select type="text" id="fieldType" name="fieldType">
-            <option></option>
+        <select id="fieldType" name="fieldType">
+            <?php foreach ($fieldsTypes as $type): ?>
+                <option value="<?php echo $type; ?>">
+                    <?php echo $type; ?>
+                </option>
+            <?php endforeach; ?>
         </select><br>
         <input type="submit" value="Submit">
     </form>
