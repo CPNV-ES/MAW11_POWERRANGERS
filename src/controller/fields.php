c64cb272529bb848b5aaa6b8b3a05bbee426113f
JuilletMikael <mikael.juillet@eduvaud.ch>
2023-10-31 21:34:05 +0100

    Feat : update to have multiple fields in a exercise

--- src/controller/fields.php
+++ src/controller/fields.php
@@ -4,7 +4,7 @@
 require_once __DIR__."/../model/fields.php";
 require_once __DIR__."/../model/fieldsTypes.php";
 
-$fields = getFieldsByExercise(1);
+$fields = getFieldsByExercise(22);
 $fieldsTypes = getFieldsTypes();
 
 // load view for exercises
