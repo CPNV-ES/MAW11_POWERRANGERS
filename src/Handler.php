bed00a75051b6151a2f4fbf4892a5b64947a697e
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2023-11-01 10:04:43 +0100

    Refactor : replace dirname(__FILE__) by __DIR__ to get the most out of php 8.2

--- src/Handler.php
+++ src/Handler.php
@@ -34,22 +34,22 @@ class Handler
     public function handle() : void
     {
         //check if handler exists and set render
-        if ($this->status_code == 200 && file_exists(dirname(__FILE__) . '/' . $this->handler . '.php'))
+        if ($this->status_code == 200 && file_exists(__DIR__ . '/' . $this->handler . '.php'))
         {
-            $this->render = dirname(__FILE__) . '/' . $this->handler . '.php';
+            $this->render = __DIR__ . '/' . $this->handler . '.php';
         }
 
         //check if error handler exists and set render
-        elseif (file_exists(dirname(__FILE__) . '/' . $this->handler . "/" . $this->status_code . ".php"))
+        elseif (file_exists(__DIR__ . '/' . $this->handler . "/" . $this->status_code . ".php"))
         {
-            $this->render = dirname(__FILE__) . '/' . $this->handler . "/" . $this->status_code . ".php";
+            $this->render = __DIR__ . '/' . $this->handler . "/" . $this->status_code . ".php";
         }
 
         //if handler doesn't exist, set render to error 500
         else
         {
             $this->status_code = 500;
-            $this->render = dirname(__FILE__) . $this->handler . "/" . $this->status_code . ".php";
+            $this->render = __DIR__ . $this->handler . "/" . $this->status_code . ".php";
         }
     }
 
