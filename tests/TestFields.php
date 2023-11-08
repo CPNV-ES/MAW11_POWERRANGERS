4c0e5055af5f3bd1451cb39af605457aaeeebdb3
JuilletMikael <mikael.juillet@eduvaud.ch>
2023-11-08 10:25:03 +0100

    Test : update fields test to get response values

--- tests/TestFields.php
+++ tests/TestFields.php
@@ -1,10 +1,15 @@
 <?php
 
+use model\class\HandlerResponse;
+use model\class\Renderer;
 use PHPUnit\Framework\TestCase;
 use model\class\DbConnector;
 
 require_once dirname(__FILE__). "/../src/model/DbConnector.php";
 require_once dirname(__FILE__).'/../src/model/fields.php';
+require_once dirname(__FILE__).'/../src/Renderer.php';
+require_once dirname(__FILE__).'/../src/HandlerResponse.php';
+
 
 // Load environment variable
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "../..");
@@ -53,53 +58,65 @@ class TestFields extends TestCase
         $fieldName = "Une question ?";
         $fieldType = 1;
         $fieldExercise = 1;
-        $expectedResult = 0;
-        $actualResult = 0;
-
-        //Connect to db
-        $bd = new DbConnector(
-            $_ENV['DATABASE_HOST'],
-            $_ENV['DATABASE_NAME'],
-            $_ENV['DATABASE_USERNAME'],
-            $_ENV['DATABASE_PASSWORD']
-        );
-
-        $resultQuery = $bd->Query(
-            "SELECT f.name AS name, ft.name AS type FROM fields f JOIN fieldTypes ft ON f.fieldTypes_id = ft.id WHERE f.exercises_id = " . $fieldExercise . ";"
-        );
-
-        //check if result is empty
-        if (!$resultQuery) {
-            $expectedResult = 0;
-        }
-
-        //Get number of field existing
-        foreach ($resultQuery as $exercise) {
-            if($exercise->name == $fieldName) $expectedResult ++;
-        }
-        //+1 cause we will inser a new value
-        $expectedResult += 1;
 
         //When
-        createField($fieldName,$fieldType,$fieldExercise);
+        $_POST["name"] = $fieldName;
+        $_POST["fieldType"] = $fieldType;
 
-        $resultQuery = $bd->Query(
-            "SELECT f.name AS name, ft.name AS type FROM fields f JOIN fieldTypes ft ON f.fieldTypes_id = ft.id WHERE f.exercises_id = " . $fieldExercise . ";"
-        );
-
-        //check if result is empty
-        if (!$resultQuery) {
-            $actualResult = 0;
-        }
-
-        //Get number of field existing
-        foreach ($resultQuery as $exercise) {
-            if($exercise->name == $fieldName) $actualResult ++;
-        }
+        new Renderer(New HandlerResponse("controller/fieldsInsert.php", 200),["exerciseId" => $fieldExercise]);
 
         //Then
-        $this->assertEquals($expectedResult, $actualResult);
+        $this->assertEquals(302, http_response_code() );
     }
+
+    /**
+     * Test Creation field if lenght of fieldname is too long
+     * @return void
+     */
+    public function testFieldCreateHandleFieldNameTooLong() {
+        //Given
+        $fieldName = "
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras gravida elit sed enim interdum, 
+        nec aliquam diam iaculis. Cras id pretium dolor. Praesent nulla enim, facilisis vitae tortor blandit, 
+        tempus posuere dolor. Praesent nec tincidunt felis. Etiam facilisis finibus arcu, id varius urna fringilla ac.
+        Quisque mollis est quis dui tincidunt, ac congue sem ullamcorper. Vivamus ut nisi vitae arcu semper semper ut ut augue.
+        Quisque nisl est, accumsan ac imperdiet sodales, ultricies sit amet ligula porttitor. Plus
+        ";
+        $fieldType = 1;
+        $fieldExercise = 1;
+
+        //When
+        $_POST["name"] = $fieldName;
+        $_POST["fieldType"] = $fieldType;
+
+        new Renderer(New HandlerResponse("controller/fieldsInsert.php", 200),["exerciseId" => $fieldExercise]);
+
+        //Then
+        $this->assertEquals(406, http_response_code() );
+
+    }
+
+    /**
+     * Test Creation field if fieldtype is empthy
+     * @return void
+     */
+    public function testFieldCreateHandleEmptyValue() {
+        //Given
+        $fieldName = "";
+        $fieldType = Null;
+        $fieldExercise = 1;
+
+        //When
+        $_POST["name"] = $fieldName;
+        $_POST["fieldType"] = $fieldType;
+
+        new Renderer(New HandlerResponse("controller/fieldsInsert.php", 200),["exerciseId" => $fieldExercise]);
+
+        //Then
+        $this->assertEquals(406, http_response_code() );
+
+    }
+
 
     /**
      * Test Creation field nominal case
