cdc528f50078f784e14f358359d3d1f4b5640665
Sebastien Moraz <sebastien@moraz.net>
2023-11-08 09:24:27 +0100

    Refactor: Update Handler test

--- tests/testHandler.php
+++ tests/testHandler.php
@@ -1,10 +1,12 @@
 <?php
 
 use model\class\Handler;
+use model\class\RouterResponse;
 use PHPUnit\Framework\TestCase;
 
 require_once dirname(__FILE__).'/../src/Handler.php';
 require_once dirname(__FILE__).'/../vendor/autoload.php';
+require_once dirname(__FILE__).'/../src/RouterResponse.php';
 
 class TestHandler extends TestCase
 {
@@ -12,12 +14,19 @@ class TestHandler extends TestCase
     private $handler = "controller/exercises";
     private $status_code = 200;
 
+    private RouterResponse $routerResponse;
+
+    protected function setUp(): void
+    {
+       $this->routerResponse = new RouterResponse($this->handler, $this->status_code);
+    }
+
     /**
      * @throws Exception
      */
     public function testInitSuccess()
     {
-        $handler = new Handler($this->handler, $this->status_code);
+        $handler = new Handler($this->routerResponse);
         $this->assertInstanceOf(Handler::class, $handler);
     }
 
@@ -26,8 +35,7 @@ class TestHandler extends TestCase
      */
     public function testHandleSuccess()
     {
-        $handler = new Handler($this->handler, $this->status_code);
-        $handler->handle();
+        $handler = new Handler($this->routerResponse);
         $this->assertIsString($handler->getRender());
         $this->assertEquals(200, $handler->getStatusCode());
     }
@@ -37,8 +45,7 @@ class TestHandler extends TestCase
      */
     public function testHandleFailed()
     {
-        $handler = new Handler("controller/errorController", $this->status_code);
-        $handler->handle();
+        $handler = new Handler(new RouterResponse("controller/exercises3", $this->status_code));
         $this->assertEquals(500, $handler->getStatusCode());
     }
 
@@ -47,8 +54,7 @@ class TestHandler extends TestCase
      */
     public function testGetRenderErrorCodesSuccess()
     {
-        $handler = new Handler("view/errors", 404);
-        $handler->handle();
+        $handler = new Handler(new RouterResponse("view/errors", 404));
         $this->assertEquals(404, $handler->getStatusCode());
     }
 }
