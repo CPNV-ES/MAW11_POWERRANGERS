fea5bc237941eb4fc07ce8a9ce9ac8a1dfdf0d08
JuilletMikael <mikael.juillet@eduvaud.ch>
2023-11-08 10:24:19 +0100

    Feat : adding response code values

--- src/controller/fieldsInsert.php
+++ src/controller/fieldsInsert.php
@@ -9,10 +9,12 @@ $fieldExercise = $this->variables['exerciseId'];
 
 if (strlen($fieldName) > 512) {
     $error_name = "The label of your field can't exceed 512 characters";
+    http_response_code(406);
     // reload view for create a new exercise
     require_once __DIR__ . "/fields.php";
 } elseif ($fieldType == null) {
     $error_select = "The value kind cannot be empty";
+    http_response_code(406);
     // reload view for create a new exercise
     require_once __DIR__ . "/fields.php";
 }else {
