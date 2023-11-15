f90a2ad8d29225336f5ca49926044a1fe0de980b
JuilletMikael <mikael.juillet@eduvaud.ch>
2023-11-15 08:53:28 +0100

    Feat : getting exercise informations

--- src/controller/fields.php
+++ src/controller/fields.php
@@ -3,9 +3,11 @@
 // load model for exercises
 require_once __DIR__."/../model/fields.php";
 require_once __DIR__."/../model/fieldsTypes.php";
+require_once __DIR__."/../model/exercises.php";
 
 $fields = getFieldsByExercise($this->variables['exerciseId']);
 $fieldsTypes = getFieldsTypes();
+$exercise = getExerciseById($this->variables['exerciseId']);
 
 // load view for exercises
 require_once __DIR__."/../view/pages/fields.php";
