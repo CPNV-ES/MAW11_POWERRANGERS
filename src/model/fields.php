a1570cda2f02e503e3ba6515bb6787e5854b7379
JuilletMikael <mikael.juillet@eduvaud.ch>
2023-11-15 10:14:17 +0100

    Fix : sql insertion on fields

--- src/model/fields.php
+++ src/model/fields.php
@@ -6,10 +6,10 @@ use model\class\DbConnector;
 require_once SOURCE_DIR . "/model/DbConnector.php";
 
 /**
- * @param $exerciseID
+ * @param int $exerciseID
  * @return array
  */
-function getFieldsByExercise($exerciseID) : array
+function getFieldsByExercise($exerciseId) : array
 {
     //initialize database connector
     $bd = new DbConnector(
@@ -19,10 +19,11 @@ function getFieldsByExercise($exerciseID) : array
         $_ENV['DATABASE_PASSWORD']
     );
 
+    $query = "SELECT f.name AS name, ft.name AS type FROM fields f JOIN fieldTypes ft ON f.fieldTypes_id = ft.id WHERE f.exercises_id = :exercise_id";
+    $queryParams["exercise_id"] = $exerciseId;
+
     //get all exercises
-    $resultQuery = $bd->Query(
-        "SELECT f.name AS name, ft.name AS type FROM fields f JOIN fieldTypes ft ON f.fieldTypes_id = ft.id WHERE f.exercises_id = " . $exerciseID . ";"
-    );
+    $resultQuery = $bd->Query($query, $queryParams);
 
     //check if result is empty
     if (!$resultQuery) {
@@ -38,10 +39,12 @@ function getFieldsByExercise($exerciseID) : array
 }
 
 /**
- * @param $fieldName
- * @param $fieldTypeId
- * @param $exerciseId
- * @return array|false
+ * Create a field
+ *
+ * @param $fieldName string name
+ * @param $fieldTypeId int type id associeted
+ * @param $exerciseId int exercise id associeted
+ * @return int associeted
  */
 function createField($fieldName,$fieldTypeId,$exerciseId)
 {
@@ -52,8 +55,32 @@ function createField($fieldName,$fieldTypeId,$exerciseId)
         $_ENV['DATABASE_PASSWORD']
     );
 
-    $query = "insert into fields (name, exercises_id, fieldTypes_id) values (?)";
-    $queryParams = [$fieldName, $exerciseId, $fieldTypeId];
+    $query = "insert into fields (name, exercises_id, fieldTypes_id) values (:name,:exercises_id,:fieldTypes_id)";
+    $queryParams = array(
+        'name' => $fieldName,
+        'exercises_id' => $exerciseId,
+        'fieldTypes_id' => $fieldTypeId
+    );
 
     return $bd->queryReturnId($query, $queryParams);
+}
+
+/**
+ * delete a field
+ *
+ * @param $id int
+ * @return string result
+ */
+function deleteField(int $id)
+{
+    $bd = new DbConnector(
+        $_ENV['DATABASE_HOST'],
+        $_ENV['DATABASE_NAME'],
+        $_ENV['DATABASE_USERNAME'],
+        $_ENV['DATABASE_PASSWORD']
+    );
+
+    $query = "DELETE FROM fields WHERE id = :id";
+    $queryParams["id"] = $id;
+    $bd->Query($query, $queryParams);
 }
