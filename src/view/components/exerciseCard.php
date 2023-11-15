1f1dab7859e2d5b835fb0df73d143740fddc2c3c
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2023-11-15 08:53:54 +0100

    Feat : add redirection from exercises list to ansers (fullfillments)

--- src/view/components/exerciseCard.php
+++ src/view/components/exerciseCard.php
@@ -4,13 +4,13 @@
  * @param string $title
  * @return void
  */
-function card(string $title) : void
+function card(array $exercise) : void
 {
     ?>
 
     <li class='card'>
-        <span class='card-title'><?= $title ?></span>
-        <button class='btn bg-purple'>take it</button>
+        <span class='card-title'><?= $exercise['name'] ?></span>
+        <a href="exercises/<?= $exercise['id'] ?>/answer" class='btn bg-purple'>take it</a>
     </li>
 
     <?php
