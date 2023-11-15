e2f0b50e3f8379ff41946ac8ccd5a40601d06d3a
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2023-11-15 09:34:53 +0100

    Feat : merge new exercise model from feature/fieldsUpdate

--- src/model/exercises.php
+++ src/model/exercises.php
@@ -36,6 +36,7 @@ function getAllExercises(): array
     return $result;
 }
 
+
 function createExercise($name): int
 {
     //initialize database connector
@@ -50,4 +51,37 @@ function createExercise($name): int
     $queryParams = [$name];
 
     return $bd->queryReturnId($query, $queryParams);
+}
+
+/**
+ * Get an exercise by passing his id
+ *
+ * @param $id integer
+ * @return array
+ */
+function getExerciseById($id): array
+{
+    //initialize database connector
+    $bd = new DbConnector(
+        $_ENV['DATABASE_HOST'],
+        $_ENV['DATABASE_NAME'],
+        $_ENV['DATABASE_USERNAME'],
+        $_ENV['DATABASE_PASSWORD']
+    );
+
+    $query = "SELECT * FROM exercises WHERE id = " . $id . ";";
+    $resultQuery = $bd->query($query);
+
+    //check if result is empty
+    if (!$resultQuery) {
+        return [];
+    }
+
+    //refactor result for view
+    foreach ($resultQuery as $exercise) {
+        $result["id"] = $exercise->id;
+        $result["name"] = $exercise->name;
+    }
+
+    return $result;
 }
