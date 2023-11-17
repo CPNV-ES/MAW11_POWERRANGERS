ebf5ab941866f543ea4adbbbf2c047115720e1f6
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2023-11-17 10:33:20 +0100

    Feat : merge all features of 'answers create' to automatically create a new fulfillment and fill it with answers

--- src/controller/fulfillmentCreate.php
+++ src/controller/fulfillmentCreate.php
@@ -1,21 +1,14 @@
 <?php
 
 // load model for exercises
-//require_once SOURCE_DIR."/model/exercises.php";
-//require_once SOURCE_DIR."/model/fields.php";
-//
-//$exercise = getExerciseById($this->variables['exerciseId']);
-//$fields = getFieldsByExercise($this->variables['exerciseId']);
-require_once SOURCE_DIR."/model/fulfillments.php";
-$fullfillment = createFulfillment();
+require_once SOURCE_DIR . "/model/fulfillments.php";
+require_once SOURCE_DIR . "/model/answers.php";
 
+$fulfillment = createFulfillment();
 
-
-foreach ($_POST as $answer=> $value){
-//    var_dump($answer);
-//    var_dump($value);
-var_dump($fullfillment);
+foreach ($_POST as $field => $value) {
+    createAnswer($value, $fulfillment, $field);
 }
 
 // load view for exercises
-require_once SOURCE_DIR."/view/pages/answerCreate.php";
+header("Location: /exercises");
