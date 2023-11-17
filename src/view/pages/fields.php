68aad9d24bb5b5797e5762ecfe0c1b14bf15b791
JuilletMikael <mikael.juillet@eduvaud.ch>
2023-11-17 09:47:29 +0100

    Feat : adding seperate component to reuse

--- src/view/pages/fields.php
+++ src/view/pages/fields.php
@@ -8,6 +8,8 @@ $navTitle = "New exercise";
 $navColor = "orange";
 $navTitle = $exercise["name"];
 
+require_once SOURCE_DIR . '/view/components/fieldRow.php';
+
 ob_start();
 ?>
 <div class="container">
@@ -25,29 +27,10 @@ ob_start();
 
                 <tbody>
                 <?php
-                foreach ($fields as $field): ?>
-                    <tr>
-                        <td class="align-middle"><?= $field->name; ?></td>
-                        <td class="text-nowrap align-middle"><?= $field->type; ?></td>
-                        <td class="align-middle">
-                            <a data-confirm="Are you sure?" title="Destroy" rel="nofollow" data-method="delete" href="/exercises/<?= $this->variables['exerciseId']; ?>/fields/<?= $field->id ?>/update">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#9b4dca"
-                                     class="bi bi-pencil-square" viewBox="0 0 16 16">
-                                    <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-                                    <path fill-rule="evenodd"
-                                          d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-                                </svg>
-                            </a>
-
-                            <a data-confirm="Are you sure?" title="Destroy" rel="nofollow" data-method="delete" href="/exercises/<?= $this->variables['exerciseId']; ?>/fields/<?= $field->id ?>/delete">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#9b4dca" class="bi bi-trash-fill" viewBox="0 0 16 16">
-                                    <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
-                                </svg>
-                            </a>
-                        </td>
-                    </tr>
-                <?php
-                endforeach; ?>
+                    foreach ($fields as $field) {
+                        row($field, $this->variables['exerciseId']);
+                    }
+                 ?>
                 </tbody>
             </table>
             <button type="button" class="btn bg-purple">
