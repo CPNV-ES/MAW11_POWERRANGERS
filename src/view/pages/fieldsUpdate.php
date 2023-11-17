c93024fb5bcbcca585bfdd657fe612834343e931
JuilletMikael <mikael.juillet@eduvaud.ch>
2023-11-17 13:12:33 +0100

    Refactor : refactor alignement of the code for lisibility

--- src/view/pages/fieldsUpdate.php
+++ src/view/pages/fieldsUpdate.php
@@ -6,44 +6,55 @@
 $title = "Exercises";
 $navTitle = "New exercise";
 $navColor = "orange";
-$navTitle = $exercise["name"];
+$navTitle = "Exercise: " . $exercise["name"];
 
 ob_start();
 ?>
 <div class="container">
     <h1>New Field</h1>
+
     <form action="/exercises/<?= $this->variables['exerciseId'] ?>/fields" method="post">
+        <!-- Name of the field  -->
         <div class="field">
             <label for="name">Label</label><br>
-            <input type="text" id="name" name="name" maxlength="512" class="form-control" <?= empty($error_name) ? "" : "class='error'" ?> value="<?= $field["name"] ?>"><br>
+            <input type="text"
+                   id="name"
+                   name="name"
+                   maxlength="512"
+                   class="form-control"
+                   <?= empty($error_name) ? "" : "class='error'" ?> value="<?= $field["name"] ?>"
+            >
+            <br>
+
+            <?php if (!empty($error_name)) : ?>
+                <span class="error-msg"><?= $error_name ?></span>
+            <?php endif; ?>
         </div>
-        <?php
-        if (!empty($error_name)) { ?>
-            <span class="error-msg"><?= $error_name ?></span>
-            <?php
-        } ?>
+        <!-- Type of the field  -->
         <div class="field">
             <label for="fieldType">Value kind</label><br>
-            <select id="fieldType" name="fieldType" class="form-control" <?= empty($error_select) ? "" : "class='error'" ?>>
-                <?php
-                foreach ($fieldsTypes as $type): ?>
-                    <option value="<?= $type->id; ?>"
-                        <?php if ($type->name = $field["type"]) : ?>
-                        selected="selected">
-                        <?php endif;?>
+            <select id="fieldType"
+                    name="fieldType"
+                    class="form-control"
+                    <?= empty($error_select) ? "" : "class='error'" ?>
+            >
 
-                    <?php
-                       echo $type->name;
-                       ?>
+                <?php foreach ($fieldsTypes as $type): ?>
+                    <option value="<?= $type->id; ?>"
+                        <?php if ($type->name == $field["type"]) : ?>
+                        selected="selected">
+                        <?php else: ?>
+                            >
+                        <?php endif;?>
+                        <?= $type->name; ?>
                     </option>
-                <?php
-                endforeach; ?>
-            </select><br>
-            <?php
-            if (!empty($error_select)) { ?>
+                <?php endforeach; ?>
+            </select>
+            <br>
+
+            <?php if (!empty($error_select)) : ?>
                 <span class="error-msg"><?= $error_select ?></span>
-                <?php
-            } ?>
+            <?php endif; ?>
         </div>
 
         <input type="submit" value="Submit" class="btn bg-purple">
