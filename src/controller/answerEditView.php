8cfde3e6f20ea988b7441862a3ce4d11d424f2ec
Mikael Juillet <142777949+JuilletMikael@users.noreply.github.com>
2023-11-22 10:22:47 +0100

    Update answerEditView.php

--- src/controller/answerEditView.php
+++ src/controller/answerEditView.php
@@ -1,13 +1,11 @@
 <?php
 
-
 // load model for exercises
 require_once SOURCE_DIR . "/model/exercises.php";
 require_once SOURCE_DIR . "/model/fields.php";
 require_once SOURCE_DIR . "/model/answers.php";
 
 $exercise = getExerciseById($this->variables['exerciseId']);
-//$fields = getFieldsByExercise($this->variables['exerciseId']);
 $fields = getAnswersByFulfillment($this->variables['answerId']);
 $description = "Bookmark this page, it's yours. You'll be able to come back later to finish.";
 
