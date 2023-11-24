fc1b078e163b89a0ff5a4ff72350c844a10625f1
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2023-11-24 09:11:08 +0100

    Fix : change form styles on fields & fieldsUpdate pages

--- src/view/pages/fields.php
+++ src/view/pages/fields.php
@@ -8,7 +8,7 @@ $navTitle = "New exercise";
 $navColor = "orange";
 $navTitle = "Exercise: " . "<strong>". $exercise["name"] . "</strong>";
 
-$styles = array("<link rel='stylesheet' href='/css/pages/fields.css'>");
+$styles = array("<link rel='stylesheet' href='/css/pages/exercise-new.css'>");
 ob_start();
 ?>
 <div class="container">
@@ -34,24 +34,27 @@ ob_start();
                         <td class="align-middle">
                             <a data-confirm="Are you sure?"
                                title="Edit"
+                               class="icon"
                                rel="nofollow"
                                data-method="put"
                                href="/exercises/<?= $this->variables['exerciseId']; ?>/fields/<?= $field->id ?>"
                             >
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                                     class="bi bi-pencil-square" viewBox="0 0 16 16">
+                                     class="bi bi-pencil-square" viewBox="0 0 16 16" fill="currentColor">
                                     <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
                                     <path fill-rule="evenodd"
                                           d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
                                 </svg>
+
                             </a>
                             <a data-confirm="Are you sure?"
                                title="Destroy"
+                               class="icon"
                                rel="nofollow"
                                data-method="delete"
                                href="/exercises/<?= $this->variables['exerciseId']; ?>/fields/<?= $field->id ?>/delete"
                             >
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" class="bi bi-trash-fill" viewBox="0 0 16 16" fill="currentColor">
                                     <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
                                 </svg>
                             </a>
@@ -79,14 +82,13 @@ ob_start();
                 <!-- Left side with list of fields  -->
                 <!-- Name of the field  -->
                 <div class="field">
-                    <label for="name">Label</label><br>
+                    <label for="name">Label</label>
                     <input type="text"
                            id="name"
                            name="name"
                            maxlength="512"
                            class="form-control" <?= empty($error_name) ? "" : "class='error'" ?>
                     >
-                    <br>
 
                     <?php if (!empty($error_name)) : ?>
                         <span class="error-msg">
--- src/view/pages/fieldsUpdate.php
+++ src/view/pages/fieldsUpdate.php
@@ -8,7 +8,7 @@ $navTitle = "New exercise";
 $navColor = "orange";
 $navTitle = "Exercise: " . "<strong>". $exercise["name"] . "</strong>";
 
-$styles = array("<link rel='stylesheet' href='/css/pages/fields.css'>");
+$styles = array("<link rel='stylesheet' href='/css/pages/exercise-new.css'>");
 ob_start();
 ?>
 <div class="container">
