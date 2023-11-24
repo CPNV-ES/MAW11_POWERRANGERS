046fefe4dce6d037808a4036d23c5b2c65e1ac8a
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2023-11-24 10:37:10 +0100

    Refactor : replacing map by foreach in answers page -> fields selector

--- src/view/pages/answer.php
+++ src/view/pages/answer.php
@@ -9,25 +9,29 @@ $styles = array("<link rel='stylesheet' href='/css/pages/exercise-new.css'>");
 ob_start();
 ?>
     <h1>Your take</h1>
-    <span><?=$description ?></span>
+    <span><?= $description ?></span>
 
     <form method="POST">
         <?php
-        array_map(function ($field) {
+        foreach ($fields as $field): ?>
+            <label><?= $field->name ?></label>
+
+            <?php
             switch ($field->length) {
-                case 64:
-                    $input = '<input type="text" name='.$field->id.' value="'.$field->value.'">';
+                case FIELD_SINGLE_LINE:
+                    echo '<input type="text" name=' . $field->id . ' value="' . $field->value . '">';
                     break;
-                case 128:
-                    $input = '<textarea name='.$field->id.'>'.$field->value.'</textarea>';
+                case FIELD_LIST_OF_SINGLE_LINE:
+                    echo '<textarea name=' . $field->id . '>' . $field->value . '</textarea>';
                     break;
-                case 255:
-                    $input = '<textarea name='.$field->id.'>'.$field->value.'</textarea>';
+                case FIELD_MULTI_LINE:
+                    echo '<textarea name=' . $field->id . '>' . $field->value . '</textarea>';
+                    break;
+                default :
+                    echo '<input type="text" name=' . $field->id . ' value="' . $field->value . '">';
                     break;
             }
-
-            echo '<label>' . $field->name . '</label>' . $input;
-        }, $fields)
+        endforeach;
         ?>
 
         <input type="submit" value="save" class="btn bg-purple">
