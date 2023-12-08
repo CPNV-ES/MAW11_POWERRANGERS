1a1d0ed55d87362e047701c502ceaf86876994be
JuilletMikael <mikael.juillet@eduvaud.ch>
2023-12-08 10:29:38 +0100

    Feat : update fullfilments model to get fullfilments by exercise

--- src/Model/Service/Fulfillments.php
+++ src/Model/Service/Fulfillments.php
@@ -34,4 +34,21 @@ class Fulfillments
         $queryParams = [gmdate("Y-m-d H:i:s")];
         return $bd->queryReturnId($query, $queryParams);
     }
+
+    public static function getFulfillmentsByExerciseId($exerciseId)
+    {
+        $bd = self::DBConnection();
+
+        $query =
+            "
+                SELECT DISTINCT f.id AS fulfillment_id, f.dateTime
+                FROM `exercise-looper`.`fulfillments` AS f
+                JOIN `exercise-looper`.`answers` AS a ON f.id = a.fulfillments_id
+                JOIN `exercise-looper`.`fields` AS fld ON a.fields_id = fld.id
+                JOIN `exercise-looper`.`exercises` AS e ON fld.exercises_id = e.id
+                WHERE e.id = (?);
+            ";
+        $queryParams = [$exerciseId];
+        return $bd->query($query, $queryParams);
+    }
 }
