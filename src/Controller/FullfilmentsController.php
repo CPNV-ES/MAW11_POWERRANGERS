072c89c4f0aaee7bf27037e7bd602908299e632d
SebastienMoraz <sebastien@moraz.net>
2023-12-08 10:14:51 +0100

    feat: add fulfillments result view

--- src/Controller/FullfilmentsController.php
+++ src/Controller/FullfilmentsController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Model\Service\Answers;
+use App\Model\Service\Exercises;
+use App\Model\Service\Fields;
 use App\Model\Service\Fulfillments;
 
 class FullfilmentsController extends Controller
@@ -17,6 +19,17 @@ class FullfilmentsController extends Controller
         }
 
         header("Location: /exercises/" . $exerciseId . "/answer/" . $fulfillment . "/edit");
+    }
+
+    public function show()
+    {
+        $fulfillmentId = $this->variables['fulfillmentsId'];
+        $fulfillment = Fulfillments::getFulfillmentById($fulfillmentId);
+        $exerciseId = $this->variables['exerciseId'];
+        $exercise = Exercises::getExerciseById($exerciseId);
+        $fields = Fields::getFieldsByExercise($exerciseId);
+        $answers = Answers::getAnswersByFulfillment($fulfillmentId);
+        require_once SOURCE_DIR . "/view/pages/fulfillments.php";
     }
 
 }
---
+++ src/view/pages/fulfillments.php
@@ -0,0 +1,24 @@
+<?php
+
+//initialize page variables
+$title = "Fullfillments";
+$navColor = "green";
+$navTitle = "Exercise: " . "<strong>". $exercise["name"] . "</strong>";
+$styles = ["<link rel='stylesheet' href='/css/pages/fulfillments.css'>"];
+ob_start();
+?>
+    <div class="container">
+        <h1><?=$fulfillment->dateTime?></h1>
+        <dl>
+            <?php foreach ($answers as $answer): ?>
+                <dt><?= $answer->name; ?></dt>
+                <dd><?= $answer->value; ?></dd>
+            <?php endforeach; ?>
+        </dl>
+    </div>
+<?php
+
+$content = ob_get_clean();
+
+//load layout
+require SOURCE_DIR . "/view/layout.php";
