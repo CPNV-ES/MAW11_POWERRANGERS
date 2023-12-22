1ca2cd1bd2947efb238270c1cdb826ba93b27b2a
Sebastien Moraz <sebastien@moraz.net>
2023-12-22 10:10:01 +0100

    chore: add link on exercise name for fields settings view

--- src/view/pages/fieldsUpdate.php
+++ src/view/pages/fieldsUpdate.php
@@ -6,7 +6,7 @@
 $title = "Fields";
 $navTitle = "New exercise";
 $navColor = "orange";
-$navTitle = "Exercise: " . "<strong>" . htmlspecialchars($exercise['name'], ENT_QUOTES, 'UTF-8') . "</strong>";
+$navTitle = "Exercise: <a href='/exercises/" . $exercise["id"] . "/fields'>" . "<strong>" . htmlspecialchars($exercise['name'], ENT_QUOTES, 'UTF-8') . "</strong></a>";
 
 ob_start();
 ?>
