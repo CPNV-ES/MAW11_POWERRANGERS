19fff37e0c5cc4aa3985454b328df39b0983288c
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2023-12-23 21:45:20 +0100

    Fix : repare the error handling on exercise creation page

--- src/Controller/ExercisesController.php
+++ src/Controller/ExercisesController.php
@@ -46,11 +46,11 @@ class ExercisesController extends Controller
         if (empty($name)) {
             $error_name = "You have to name your exercise";
             // reload view for create a new exercise
-            require_once SOURCE_DIR . "/view/pages/exerciseCreate";
+            require_once SOURCE_DIR . "/view/pages/exerciseCreate.php";
         } elseif (strlen($name) > 96) {
             $error_name = "The name of your exercise can't exceed 96 characters";
             // reload view for create a new exercise
-            require_once SOURCE_DIR . "/view/pages/exerciseCreate";
+            require_once SOURCE_DIR . "/view/pages/exerciseCreate.php";
         } else {
             $exerciseId = Exercises::createExercise($name);
             header("Location: /exercises/" . $exerciseId . "/fields");
